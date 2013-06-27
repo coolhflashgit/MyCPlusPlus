c7ebc3576b0d22ae98fc39b9260280b518ba1218
coolhflashgit <coolherrybh@gmail.com>
2013-06-27 17:25:28 +0800

    GA

--- genetic_algorithm/run.cpp
+++ genetic_algorithm/run.cpp
@@ -12,14 +12,6 @@ int main()
 {
 	CGenAlg ca;
 	ca.StartCGenAlg();
-//	for(int i = 0;i<20;i++)
-//	{
-//		srand((unsigned)time(NULL));
-//		cout << rand() << endl;
-//	}
-//	return 0;
-
-//	cout << 1.7*sin(10*3.141592653*1.7) <<endl;
 }
 
 
