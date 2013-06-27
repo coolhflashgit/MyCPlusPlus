bb0e7450a3a70284ac5c189ecc59fa87410b5ca4
coolhflashgit <coolherrybh@gmail.com>
2013-06-27 17:24:04 +0800

    GA

---
+++ genetic_algorithm/CGenome.cpp
@@ -0,0 +1,14 @@
+/*
+ * CGenome.cpp
+ *
+ *  Created on: 2013-6-17
+ *      Author: Administrator
+ */
+#include "CGenome.h"
+
+CGenome::CGenome():dFitness(0){};
+
+CGenome::CGenome(vector<double> w,double d):vecWeight(w),dFitness(d){};
+
+
+
---
+++ genetic_algorithm/run.cpp
@@ -0,0 +1,26 @@
+/*
+ * run.cpp
+ *
+ *  Created on: 2013-6-27
+ *      Author: Administrator
+ */
+
+#include "CGenAlg.h"
+#include <math.h>
+
+int main()
+{
+	CGenAlg ca;
+	ca.StartCGenAlg();
+//	for(int i = 0;i<20;i++)
+//	{
+//		srand((unsigned)time(NULL));
+//		cout << rand() << endl;
+//	}
+//	return 0;
+
+//	cout << 1.7*sin(10*3.141592653*1.7) <<endl;
+}
+
+
+
